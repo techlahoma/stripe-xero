7910e5cf80b12e9692fe1e61de1b578733ca2a3d
Hans Kuder <hanskuder@gmail.com>
2015-02-28 11:33:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Stripe-Xero
+=================
+
+This is a simple script to create statements from your Stripe account that you can import into [Xero](http://www.xero.com/). These statements will probably work with other accounting software with minimal tweaking.
+
+Transfer transactions from this tool should match the transactions in your bank account, so reconciliation is simpler. The tool also creates separate transactions for Stripe processing fees.
+
+This tool saves the timestamp of the last balance transfer it found in Stripe, so successive runs will only include new transactions since the last run.
+
+Dependencies
+------------
+
+Stripe's Python library
+
+        pip install stripe
+
+How to Use It
+------------
+
+Run the script. You'll need to set the ``STRIPE_API_KEY`` environment variable with your Stripe account's secret key. For example:
+
+        env STRIPE_API_KEY=sk_live_abcd1234 python stripe_statements.py
+
+The script will grab all of your balance transactions and create a CSV file suitable for importing into Xero. The next time it runs, it will only grab new transactions since the last run.
+
+To get the data into Xero, you'll first need a bank account in Xero that represents your Stripe account.
+
+Import the CSV file "statement" into your Stripe bank account in Xero, confirm that the fields match up the way you expect, and start reconciling.
+
+Enjoy!
+
+Other Notes
+-------
+
+We use this script for quick-and-dirty reconciliation at [Retention Hero](http://www.retentionhero.com/). Bug reports and pull requests welcome.
---
+++ stripe_statements.py
@@ -0,0 +1,65 @@
+import os
+import pickle
+import time
+import calendar
+import csv
+import datetime
+import stripe
+
+last_txn_filename = 'last_txn_time.pkl'
+
+def process_new_transactions():
+    last_txn_time = None
+    try:
+        with open(last_txn_filename, 'rb') as last_txn_file:
+            last_txn_time = pickle.load(last_txn_file)
+    except IOError:
+        print "No previous transaction found. Getting all available transactions."
+        pass
+
+    utcnow_timestamp = calendar.timegm(time.gmtime())
+
+    page_size = 2
+    filter_params = {
+        'limit': page_size,
+    }
+    if last_txn_time:
+        filter_params['created'] = {'gt': last_txn_time}
+
+    have_more_data = True
+    balance_transactions = []
+    while have_more_data:
+        page = stripe.BalanceTransaction.all(**filter_params)
+        balance_transactions.extend(page['data'])
+
+        have_more_data = page['has_more']
+        if have_more_data:
+            filter_params['starting_after'] = page['data'][-1]['id']
+
+    print "Found %d new transactions" % len(balance_transactions)
+    if len(balance_transactions) == 0:
+        print "Not creating csv file."
+        return
+
+    balance_transactions.sort(key=lambda b: b['created'])
+    statement_filename = 'stripe_%s.csv' % utcnow_timestamp
+    with open(statement_filename, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['id', 'source', 'amount', 'currency', 'type', 'date', 'description'])
+        for tr in balance_transactions:
+            created_date = datetime.datetime.fromtimestamp(tr['created']).strftime('%Y-%m-%d')
+            writer.writerow([tr['id'], tr['source'], "%.2f" % (tr['amount'] / 100.), tr['currency'], tr['type'], created_date, tr['description']])
+            for fee in tr['fee_details']:
+                writer.writerow([tr['id'], tr['source'], "%.2f" % (fee['amount'] / 100.), fee['currency'], fee['type'], created_date, fee['description']])
+
+    print "Created %s" % statement_filename
+
+    if balance_transactions:
+        last_txn_time = max([b['created'] for b in balance_transactions])
+        with open(last_txn_filename, 'wb') as last_txn_file:
+            pickle.dump(last_txn_time, last_txn_file)
+
+
+if __name__ == '__main__':
+    stripe.api_key = os.environ['STRIPE_API_KEY']
+    process_new_transactions()
